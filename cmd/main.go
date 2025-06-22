5413ecdef1bf07fa22f3efc79e711c847bc3d9eb
Alex Langevin <alexlangev@gmail.com>
2025-06-22 11:17:46 -0400

    project structure maybe

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	direct := flag.Bool("c", false, `Create worklog directly`)
+
+	flag.Parse()
+
+	switch {
+	case *direct:
+		fmt.Println("CLI here!")
+		// env incomplete?
+		// validate Jira auth with currentUser()
+		// print valid auth or not
+
+	default:
+		// TODO setup tui
+		// tui.Run()
+		fmt.Println("The TUI implementation is not ready.")
+	}
+}
